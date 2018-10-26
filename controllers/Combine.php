e8b002a4674057df120adc08d439496e0b29a62d
Alexandr Ablizin <mcmraak@gmail.com>
2018-10-26 19:06:04 +0400

    functional extension

--- controllers/Combine.php
+++ controllers/Combine.php
@@ -13,10 +13,25 @@ class Combine
             $css_combain = $theme_path.'/assets/css/combine.css';
             $css_files = file($theme_path.'/meta/css.txt');
             $combain = '';
-            foreach($css_files as $css)
-            {
-                $combain .= self::minify(file_get_contents($theme_path.'/'.trim($css)));
+
+            $path_list = [];
+            foreach ($css_files as $css){
+                if(preg_match('/^#.*/', $css)) continue;
+                $css_file_path = $theme_path.'/'.trim($css);
+                $path_list[$css_file_path]=null;
             }
+
+            foreach($path_list as $css=>$null)
+            {
+                if(file_exists($css)) {
+                    $combain .= file_get_contents($css);
+                } else {
+                    echo "<div style='text-align:center;color:#fff;background:red'>No such file $css</div>";
+                }
+            }
+
+            $combain = self::minify($combain);
+
             file_put_contents($css_combain, $combain);
             return [
                 'css_list' => $css_files,
@@ -32,6 +47,7 @@ class Combine
 
     public static function minify($css)
     {
+        if(!$css) return;
         return str_replace('; ',';',
                str_replace(' }','}',
                str_replace('{ ','{',
